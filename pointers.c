a7e8ced8dfc24683a3929e818b25a21a03629f0f
Arthur Araujo <arthur.araujo@tutanota.com>
2026-02-21 23:37:20 -0300

    pointers.c

--- pointers.c
+++ pointers.c
@@ -1,3 +1,17 @@
-int main() {
+#include <stdio.h>
 
+int main() {
+    int a[10];
+
+    a[0] = 1;
+    a[1] = 2;
+    a[2] = 3;
+
+    // int *pa = &a[2];
+
+    // int x = *pa-1;
+
+    int *pa = a;
+
+    printf("x is %i", *pa+1);
 }
